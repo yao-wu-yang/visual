b5e225cdec493ece5105f047a38df1e1f8edfcaa
FishC_Service <fishc_service@126.com>
2023-12-08 12:05:33 +0800

    Final

---
+++ film/doubtop10/doub.js
@@ -0,0 +1,45 @@
+(function ($) {
+    $.fn.top = function () {
+        var selectors = {
+            id: $(this),
+            item: $(this).find(".item"),
+            activeClass: "item--active",
+            img: ".img"
+        };
+        selectors.item.eq(0).addClass(selectors.activeClass);
+        selectors.id.css(
+            "background-image",
+            "url(" +
+            selectors.item.first()
+                .find(selectors.img)
+                .attr("src") +
+            ")"
+        );
+        var itemLength = selectors.item.length;
+        $(window).scroll(function () {
+            var max, min;
+            var pos = $(this).scrollTop();
+            selectors.item.each(function (i) {
+                min = $(this).offset().top;
+                max = $(this).height() + $(this).offset().top;
+                var that = $(this);
+                if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+                    selectors.item.removeClass(selectors.activeClass);
+                    selectors.id.css("background-image", "url(" + selectors.item.last().find(selectors.img).attr("src") + ")");
+                    selectors.item.last().addClass(selectors.activeClass);
+                }
+                else if (pos <= max - 10 && pos >= min) {
+                    selectors.id.css("background-image",
+                        "url(" +
+                        $(this)
+                            .find(selectors.img)
+                            .attr("src") +
+                        ")");
+                    selectors.item.removeClass(selectors.activeClass);
+                    $(this).addClass(selectors.activeClass);
+                }
+            });
+        });
+    };
+})(jQuery)
+$("#shell").top();
